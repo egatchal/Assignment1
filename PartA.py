9988fbd07236cb665af3bff244670502e7fa85d7
Einar Gatchalian <egatchal@uci.edu>
2024-04-08 00:07:07 -0700

    PartA redone to read line by line (not entire text file)

--- PartA.py
+++ PartA.py
@@ -16,26 +16,24 @@ def tokenize(textFilePath):
     
     try:
         with open(textFilePath, "r") as f:
-            text = f.read().lower()
+            tokens = []
+            for line in f:
+                token = ""
+                line = line.lower()
+                for c in line:
+                    if (c.isalnum()): # check if character is alphanumeric and add to token
+                        token += c
+                    elif (token == ""): # if token is empty string do not add to tokens
+                        continue
+                    else:
+                        tokens.append(token) # if token is not empty string add to tokens
+                        token = ""
+                
+                if token != "": # if last token is not empty string add to tokens
+                    tokens.append(token)
+            return tokens
     except FileNotFoundError:
         return []
-    
-    tokens = []
-    token = ""
-    
-    for c in text:
-        if (c.isalnum()): # check if character is alphanumeric and add to token
-            token += c
-        elif (token == ""): # if token is empty string do not add to tokens
-            continue
-        else:
-            tokens.append(token) # if token is not empty string add to tokens
-            token = ""
-    
-    if token != "": # if last token is not empty string add to tokens
-        tokens.append(token)
-    
-    return tokens
 
 def computeWordFrequencies(tokens):
     """Compute the frequency of tokens
@@ -69,7 +67,7 @@ def printFrequencies(frequencies):
 
 if __name__ == "__main__":
     tokens = tokenize("/Users/einargatchalian/Downloads/sheesh.txt")
-    #print(tokens)
+    print(len(tokens))
     frequencies = computeWordFrequencies(tokens)
     #print(frequencies)
     printFrequencies(frequencies)
